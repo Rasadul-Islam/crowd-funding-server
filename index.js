bb3d1befa11788d7eabbe5be6b9db70729ee8a3a
Rasadul Islam <rasadulislam.cse@gmail.com>
2024-12-15 12:10:20 +0600

    set database with new campaign form

--- index.js
+++ index.js
@@ -25,6 +25,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // const database = client.db("insertDB");
+    // const haiku = database.collection("haiku");
+    const campaignCollection = client.db('campaignDB').collection('campaign');
+
+    app.post('/campaign', async(req, res)=>{
+      const newCampaign = req.body;
+      console.log(newCampaign);
+      const result = await campaignCollection.insertOne(newCampaign);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

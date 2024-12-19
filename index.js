45ce8272e2c0326fa720f76712b8401dc1ccb0fe
Rasadul Islam <rasadulislam.cse@gmail.com>
2024-12-20 01:37:14 +0600

    added delete campaign

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -40,6 +40,13 @@ async function run() {
     app.get('/campaign', async (req, res) => {
       const cursor = campaignCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.delete('/campaign/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query ={_id: new ObjectId(id)}
+      const result =await campaignCollection.deleteOne(query);
       res.send(result);
     })
 

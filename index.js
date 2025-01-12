92074322a72e177b825bd37c361d78c07a4b6795
Rasadul Islam <rasadulislam.cse@gmail.com>
2025-01-13 02:09:03 +0600

    check and correction

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function run() {
     //post campaign data
     app.post('/campaign', async (req, res) => {
       const newCampaign = req.body;
-      console.log(newCampaign);
+      // console.log(newCampaign);
       const result = await campaignCollection.insertOne(newCampaign);
       res.send(result);
     })
@@ -55,7 +55,7 @@ async function run() {
           .find({deadline: { $gte: currentDate.toISOString()},}).limit(6).toArray();
         res.send(activeCampaigns); 
       } catch (error) {
-        console.error(error);
+        // console.error(error);
         res.status(500).send({ message: 'Error fetching campaigns.' });
       }
     });
@@ -131,7 +131,7 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
